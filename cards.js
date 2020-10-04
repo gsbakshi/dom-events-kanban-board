721358776cb699b76d3ba6dcb77503b9141731b9
gsbakshi <gurubakshi486@gmail.com>
2020-10-04 19:00:45 +0530

    input functionality fixed //cleanup req

---
+++ cards.js
@@ -0,0 +1,5 @@
+var newCard = document.getElementById("new-card");
+
+newCard.addEventListener("click", function() {
+    console.log("New Card");
+})
--- script.js
+++ script.js
@@ -1,64 +1,152 @@
-var button = document.getElementById("enter");
+var button = document.getElementById("add-task");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var items = document.getElementsByTagName("li");
-// var deleteBtn = document.getElementById("delete");
+
+var checkbox = document.querySelectorAll(".checkbox");
+var li = document.querySelectorAll("li");
+var item = document.querySelectorAll(".item");
+var deleteBtn = document.getElementById(".delete");
+
+// <!-- var items = document.getElementsByTagName("li"); -->
+// <!-- var deleteBtn = document.getElementById("delete"); -->
+
+function updateInput() {
+	return checkbox = document.querySelectorAll(".checkbox");
+}
+
+function updateLi() {
+	return li = document.querySelectorAll("li");
+}
+
+function updateItem() {
+	return item = document.querySelectorAll(".item");
+}
+
+function updateDelete() {
+	return deleteBtn = document.querySelectorAll(".delete");
+}
+
+function updateAll() {
+	updateDelete();
+	updateInput();
+	updateItem();
+	updateLi();
+	deleteList();
+	toggleList();
+}
+
+function deleteList() {
+	for(i=0; i<deleteBtn.length; i++) {
+		deleteBtn[i].addEventListener("click", removeListItem);
+	}
+}
+
+function removeListItem() {
+	this.parentNode.remove();
+}
+
+function toggleList()	{
+	for (let i = 0; i < checkbox.length; i++) {
+		checkbox[i].addEventListener("click", toggle);
+	}
+}
+
+function toggle()	{
+	this.parentNode.children[1].classList.toggle("done");
+}
 
 
 function inputLength() {
 	return input.value.length;
 }
 
-function itemsLength() {
-	return items.length;
-}
-
-function removeParent(event){
-	event.target.parentNode.remove();
-}
-
-function deleteButton() {
-	var del= document.createElement("button");
-	btn.appendChild(document.createTextNode("Delete"));
-	items[i].appendChild(btn);
-	btn.onclick = removeParent;
-}
-
-for(var i = 0 ; i < itemsLength; i++) {
-	deleteButton();
-}
-
-function strikeThrough(event) {
-	if(event.target.tagName === "li") {
-		target.classList.toggle("done");	
-	}
-}
-
 function createListElement() {
-	var btn = document.createElement("button");
-	btn.innerHTML = "Delete";
-
+	var div = document.createElement("div");
+	var createCheck = document.createElement("input");
+	var createDelete = document.createElement("button");
 	var li = document.createElement("li");
+	div.classList.add("item");
+	createCheck.classList.add("checkbox");
+	createDelete.classList.add("delete");
+	createCheck.type = "checkbox";
+	div.appendChild(createCheck);
+	div.appendChild(li);
+	div.appendChild(createDelete);
 	li.appendChild(document.createTextNode(input.value));
-	li.innerHTML=li.innerHTML + "  ";
-	li.appendChild(btn);
-	
-	ul.appendChild(li);
+	createDelete.appendChild(document.createTextNode("Delete"));
+	ul.appendChild(div);
 	input.value = "";
 }
 
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
+		updateAll();
 	}
 }
 
 function addListAfterKeypress(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
+		updateAll();
 	}
 }
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+updateAll();
+
+// <!-- Original Code -->
+// <!-- function itemsLength() {
+// 	return items.length;
+// }
+
+// function removeParent(event){
+// 	event.target.parentNode.remove();
+// }
+
+// function deleteButton() {
+// 	var del= document.createElement("button");
+// 	btn.appendChild(document.createTextNode("Delete"));
+// 	items[i].appendChild(btn);
+// 	btn.onclick = removeParent;
+// }
+
+// for(var i = 0 ; i < itemsLength; i++) {
+// 	deleteButton();
+// }
+
+// function inputLength() {
+// 	return input.value.length;
+// }
+
+// function createListElement() {
+// 	var btn = document.createElement("button");
+// 	btn.appendChild(document.createTextNode('Delete'));
+// 	btn.setAttribute("class", "btn btn-dark");
+
+// 	var li = document.createElement("li");
+// 	li.appendChild(document.createTextNode(input.value));
+// 	li.appendChild(document.createTextNode(" "));
+// 	li.appendChild(btn);
+
+// 	ul.appendChild(li);
+// 	input.value = "";
+// }
+
+// function addListAfterClick() {
+// 	if (inputLength() > 0) {
+// 	createListElement();
+// 	}
+// }
+
+// function addListAfterKeypress(event) {
+// 	if (inputLength() > 0 && event.keyCode === 13) {
+// 	createListElement();
+// 	}
+// }
+
+// button.addEventListener("click", addListAfterClick);
+// input.addEventListener("keypress", addListAfterKeypress); -->
